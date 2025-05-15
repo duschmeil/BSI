fe6123d236e0d09bde5ae12b47f6e108982521e4
eduschmeil <eduschmeil@gmail.com>
2025-05-14 21:28:18 -0300

    ex semana 10

--- RA/ex semana 10/exercicio10.py
+++ RA/ex semana 10/exercicio10.py
@@ -1,4 +1,15 @@
 vetor = [2, 4, 7, 2, 3, 3, 1, 0, 2, 6]
-contagens = 
 
-for num in vetor:
+mais_repetido = vetor[0]
+maior_contagem = 0
+
+for i in range(len(vetor)):
+    contagem = 0
+    for j in range(len(vetor)):
+        if vetor[i] == vetor[j]:
+            contagem += 1
+    if contagem > maior_contagem:
+        maior_contagem = contagem
+        mais_repetido = vetor[i]
+
+print(f"Número que mais se repete: {mais_repetido}")
